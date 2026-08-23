3657c813bffd953db1a49f3dbce2d10fb5047efb
GajulaNazia-25311a6741 <25311a6741@ds.sreenidhi.edu.in>
2026-08-23 10:28:18 +0530

    Add files via upload
    Add Travel Destination website

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function showMessage() {
+    alert("🌍 Get ready for your next adventure! ✈️");
+}
+
+function showDestination(Ooty) {
+    alert(
+        "Welcome to " + Ooty + "! 🌴\n\n" +
+        "Get ready to explore this amazing destination!"
+    );
+}
